2032f87f429f5e18ebf6f80af97ae5b84c36db4d
malikasad517 <156124525+malikasad517@users.noreply.github.com>
2026-03-02 06:05:16 +0500

    Fix video source filename case sensitivity

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -137,7 +137,7 @@ export default function HeroSection() {
         <div className="mt-8 lg:-mt-48 xl:-mt-60 relative z-10">
           <video
             ref={videoRef}
-            src="/Hero_vid.mp4"
+            src="/Hero_Vid.mp4"
             autoPlay
             loop
             muted
